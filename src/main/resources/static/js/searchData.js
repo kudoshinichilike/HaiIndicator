8be09793cb9de0f4a6626d5c2289008695da50ed
Hoàng Nguyên <nguyenhd01012000@gmail.com>
2023-09-30 00:35:18 +0700

    add table 1

--- src/main/resources/static/js/searchData.js
+++ src/main/resources/static/js/searchData.js
@@ -1,15 +1,10 @@
-var metric_items = {
-    "ThoiGian": "Thời gian",
-    "Gia": "Giá",
-    "GiaThayDoi": "Giá thay đổi",
-    "KLLo": "KL theo lô",
-    "KLTichLuy": "KL tích lũy",
-    "TiTrong": "Tỷ trọng"
-}
-
-var items = []
-
-var fields = []
+var fields = [
+    { key: 'time', label: 'Thời gian', sortable: true },
+    { key: 'price', label: 'Giá', sortable: true },
+    { key: 'matchVolume', label: 'KL theo lô', sortable: true },
+    { key: 'accumulatedVolume', label: 'KL tích lũy', sortable: true },
+    { key: 'proportion', label: 'Tỷ trọng', sortable: true },
+]
 
 var viewSearchLog = new Vue({
     el: '#view-search-data',
@@ -17,63 +12,54 @@ var viewSearchLog = new Vue({
         Multiselect: window.VueMultiselect.default
     },
     data: {
+        items: [],
         visible: true,
-        code: '',
+        code: 'CTG',
         dateSearch: '',
         isSearching: false,
         errored: false,
-        fab: conf_fab
+        fab: conf_fab,
+        rows: 0,
+        perPage: 10,
+        currentPage: 1,
+
+        selected: null,
+        options: [
+            { value: null, text: 'Please select an option' },
+            { value: 'a', text: 'This is First option' },
+            { value: 'b', text: 'Selected Option' },
+            { value: { C: '3PO' }, text: 'This is an option with object value' },
+            { value: 'd', text: 'This one is disabled', disabled: true }
+        ]
     },
     methods: {
         onSearchClicked() {
             var isSearching = true
-            this.makeToast('warning')
-
-//                            "code": this.code,
-//                            "dateSearch": this.dateSearch
-
-            console.log('onSearchClicked, code = ' + this.code + ', dateSearch=' + this.dateSearch)
-            axios
-                .post('api/haiIndicator/searchData', {
-//                    code: this.code,
-//                    dateSearch: this.dateSearch,
-                    code: "FPT",
-                    dateSearch: "9/22/2023",
-                })
-                .then(res => {
-                    var data = res.data
-                    console.log("res data: " + JSON.stringify(data))
-//                    var _fields = []
-//                    var _items = []
-//                    var _maxColumn = 0;
-//                    data.forEach((item) => {
-//                        var columns = item.split("|")
-//                        _maxColumn = Math.max(_maxColumn, columns.length)
-//                        var item = {}
-//                        item["metric"] = columns[0]
-//                        item["time"] = columns[1]
-//                        for (var i = 2; i < columns.length; i++)
-//                            item['column' + i] = columns[i]
-//                        _items.push(item)
-//                    })
-//                    _fields.push({key: "metric", sortable: true})
-//                    _fields.push({key: "time", sortable: true})
-//                    for (var i = 2; i < _maxColumn; i++)
-//                        _fields.push({key: 'column' + i, sortable: true})
-//
-//                    this.fields = _fields
-//                    this.items = _items
-//                    this.totalRows = this.items.length
-                })
-                .catch(e => {
-                    console.log(e)
-                })
-                .finally(() => this.isSearching = false)
+            if (isEmpty(this.code)) this.makeToast('danger', 'Bạn chưa nhập mã chứng khoán')
+            else {
+                axios
+                    .post('api/haiIndicator/searchData', {
+                        code: this.code,
+                        dateSearch: this.dateSearch,
+                        dataSource: "CAFE_F"
+                    })
+                    .then(res => {
+                        var data = res.data.matchData
+                        this.items = data
+                        this.items.forEach(i => i.price = i.price + i.changePrice)
+                        this.rows = this.items.length
+                        })
+                    .catch(e => {
+                        console.log(e)
+                        this.makeToast('danger', e)
+                    })
+                    .finally(() => this.isSearching = false)
+            }
         },
 
-        makeToast(variant = null) {
-            this.$bvToast.toast('Tìm full log của user trong nhiều ngày sẽ chậm, và có thể không cần thiết. Nhập những "tag" thực sự cần tìm để đạt hiệu quả tốt nhất', {
-                title: 'Warning',
+        makeToast(variant = null, msg) {
+            this.$bvToast.toast(msg, {
+                title: 'Thông báo',
                 variant: variant,
                 solid: true,
                 autoHideDelay: 8000
@@ -83,5 +69,6 @@ var viewSearchLog = new Vue({
     },
     created: function () {
         this.dateSearch = dateToString(new Date())
+        this.onSearchClicked()
     },
 })
